03614b7979a7c32cb6e56ab1c5b7061462e51b32
Tailung1 <ramazsamitashvili11@gmail.com>
2025-06-28 18:22:26 +0400

    feat: create .env file to secure postgre configuration data

--- config/db.config.js
+++ config/db.config.js
@@ -1,12 +1,14 @@
 import pkg from "pg";
 const { Pool } = pkg;
+import dotenv from "dotenv";
+dotenv.config();
 
 const pool = new Pool({
-  user: "postgres",
-  password: "2003",
-  host: "localhost",
-  port: 5432,
-  database: "Ecommerce",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 export default pool;
