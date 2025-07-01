48afdececaec65b059a30031859ca2bb12c34601
Tailung1 <ramazsamitashvili11@gmail.com>
2025-07-01 21:14:07 +0400

    set up prisma delete functional

--- controllers/productController.js
+++ controllers/productController.js
@@ -78,16 +78,22 @@ async function deleteProdct(req, res) {
   const { id } = req.params;
 
   try {
-    const result = await pool.query(
-      "DELETE FROM products WHERE id = $1 RETURNING *",
-      [id]
-    );
-    if (result.rows.length === 0) {
-      return res.status(404).json({ error: "Product not found" });
+    // const result = await pool.query(
+    //   "DELETE FROM products WHERE id = $1 RETURNING *",
+    //   [id]
+    // );
+    // if (result.rows.length === 0) {
+    //   return res.status(404).json({ error: "Product not found" });
+    // }
+    const deletedProduct = await prisma.products.delete({
+      where: { id: parseInt(id) },
+    });
+    if (!deleteProdct) {
+      res.status(404).json({ message: "Product not found" });
     }
     res
       .status(200)
-      .json({ message: "Product deleted", product: result.rows[0] });
+      .json({ message: "Product deleted", product: deletedProduct });
   } catch (err) {
     console.error("Error deleting product", err.stack);
     res.status(500).json({ error: "Internal server error" });
