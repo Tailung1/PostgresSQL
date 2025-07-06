28e3c1dd7efb1e01b958ee025e72077fcfaa741d
Tailung1 <ramazsamitashvili11@gmail.com>
2025-07-06 21:00:07 +0200

    modify prisma schema

--- controllers/userProductsController.js
+++ controllers/userProductsController.js
@@ -1,8 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-async function buyProduct(req, res) {
-    
-}
+async function buyProduct(req, res) {}
 
-export {buyProduct}
+export { buyProduct };
