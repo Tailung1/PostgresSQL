8dc3fbf08c1cf1130ea991d0766631bfb55630b6
Tailung1 <ramazsamitashvili11@gmail.com>
2025-07-09 20:39:02 +0200

    init prisma migrate

---
+++ prisma/migrations/20250709183342_init/migration.sql
@@ -0,0 +1,32 @@
+-- CreateTable
+CREATE TABLE "category" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" VARCHAR(255),
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" VARCHAR(255),
+    "categoryID" INTEGER NOT NULL DEFAULT 1,
+
+    CONSTRAINT "untitled_table_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "firstName" VARCHAR(255),
+    "lastName" VARCHAR(255),
+    "email" VARCHAR(255),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_categoryID_fkey" FOREIGN KEY ("categoryID") REFERENCES "category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
