487b5630a99028a3ea98466b2c30217a47a4bc8a
Tailung1 <ramazsamitashvili11@gmail.com>
2025-07-15 22:20:09 +0200

    feat: add catch functionality to main func

--- prisma/seed.js
+++ prisma/seed.js
@@ -15,21 +15,28 @@ async function main() {
     },
   });
   await prisma.products.createMany({
-    data:[
-        {
-            name:"iPhone 15",
-            price:999.99,
-            stock:20,
-            slug:"iphone-15",
-            categoryID:electronics.id
-        },
-        {
-            name:"Samsung Galaxy S24",
-            price:779.99,
-            stock:15,
-            slug:"samsung galaxy s24",
-            categoryID:electronics.id
-        }
-    ]
-  })
+    data: [
+      {
+        name: "iPhone 15",
+        price: 999.99,
+        stock: 20,
+        slug: "iphone-15",
+        categoryID: electronics.id,
+      },
+      {
+        name: "Samsung Galaxy S24",
+        price: 779.99,
+        stock: 15,
+        slug: "samsung galaxy s24",
+        categoryID: electronics.id,
+      },
+    ],
+  });
+  console.log("Sending completed");
 }
+main()
+  .catch((e) => {
+    console.error("Sening failed", e);
+    process.exit(1);
+  })
+  .finally(() => prisma.$disconnect());
