451ee37fa4272219d5579bd7c55e185a80dae2d5
Tailung1 <ramazsamitashvili11@gmail.com>
2025-07-21 20:53:13 +0200

    delete router

--- routes/buyProductsRouters.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { buyProduct } from "../controllers/productController";
-
-const buyProductsRoter = express.Router();
-
-buyProductsRoter.route("/id").post(buyProduct);
