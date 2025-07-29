a8ff1f9ff9cbd8cb69801ef566b9b2e232d704ec
Tailung1 <ramazsamitashvili11@gmail.com>
2025-07-29 21:18:20 +0200

    init 29 july

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 
 export const auth = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1]
-  
+  const token = req.headers.authorization.split(" ")[1];
+
   if (!token) {
     return res.status(401).json({ message: "Unauthorized1" });
   }
