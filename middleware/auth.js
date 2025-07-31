03780fc9ccfcc6a618bebb8be6d564812b7ba506
Tailung1 <ramazsamitashvili11@gmail.com>
2025-07-31 17:46:12 +0200

    modify auth display

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const auth = (req, res, next) => {
-  const token = req.headers.authorization
+  const token = req.headers.authorization;
   if (!token) {
     return res.status(401).json({ message: "Unauthorized1" });
   }
