a17c6e584f0aa703e7b78b8837d76fc91caf8a4e
Tailung1 <ramazsamitashvili11@gmail.com>
2025-08-25 18:45:10 +0200

    add isOperational check

--- utils/errorhander.js
+++ utils/errorhander.js
@@ -13,7 +13,14 @@ export class AppError extends Error {
 export const handleError = (err, req, res, next) => {
   (err.statusCode = err.statusCode || 500),
     (err.status = err.status || "error");
-  if ("NODE_ENV=development") {
+
+  if (!err.isOperational) {
+    return res
+      .status(500)
+      .json({ status: "error", message: "Internal server error" });
+  }
+
+  if (process.env.NODE_ENV === "development") {
     return res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
